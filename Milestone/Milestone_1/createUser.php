ddcc6156d1f260c7f465f003cce9fbebebb2a9f3
Matt Sievers <msievers2@my.gcu.edu>
2019-06-14 20:37:34 -0500

    Final commit for MS-1 User Registration Page
    created a createDB.sql file for needed scripts to create tables needed for this milestone.
    refactored the userRegistrationPage to flow with the schema for the user_info table.
    removed role items as they're un needed for this deliverable.
    refactored createUser.php to add logic for required fields as well as the needed logic for nickName per requirements.
    Added throw sql conn error to creation of user for better troubleshooting.
    removed un needed createDB.php
    added folder for Project Reports.

--- Milestone/Milestone_1/createUser.php
+++ Milestone/Milestone_1/createUser.php
@@ -7,67 +7,56 @@
 <h1>New User Created Results</h1>
 <?php
 
+$userName = $_POST['userName'];
+$firstName = $_POST['firstName'];
+$middleName = $_POST['middleName'];
+$lastName = $_POST['lastName'];
+$nickName = $_POST['nickName'];
+$email1 = $_POST['email1'];
+$email2 = $_POST['email2'];
+$address1 = $_POST['address1'];
+$address2 = $_POST['address2'];
+$city = $_POST['city'];
+$state = $_POST['state'];
+$zipCode = $_POST['zipCode'];
+$country = $_POST['country'];
 
-$dbUserName = 'testUser';
-$dbPass = 'cst126pass';
-$dbName = 'cst126milestone';
+if (empty($nickName)) $nickName = $firstName . $lastName;
 
-$firstName = $_POST['First Name'];
-$middleName = $_POST['Middle Name'];
-$lastName = $_POST['Last Name'];
-$nickName = $_POST['Nick Name'];
-$userName = $_POST['User Name'];
-$address1 = $_POST['Address1'];
-$address2 = $_POST['Address2'];
-$email1 = $_POST['Email'];
-$email2 = $_POST['Alternate Email'];
-$city = $_POST['City'];
-$state = $_POST['State'];
-$zipCode = $_POST['ZipCode'];
-$country = $_POST['Country'];
-$userRole = $_POST['User Role'];
-$userRoleId = 0;
+if (!isset($firstName) || trim($firstName) == '') {
+    echo "First Name is required, please try again.<br />";
+}
 
-if (!isset($_POST['First Name']) || !isset($_POST['Last Name'])
-    || !isset($_POST['Email']) || !isset($_POST['User Role']) || !isset($_POST['User Name'])) {
-    echo "<p>You have not entered all required fields, <br />
-    Please try again.</p>";
+if (!isset($lastName) || trim($lastName) == '') {
+    echo "Last Name is required, please try again.<br />";
+}
+
+if (!isset($userName) || trim($userName) == '') {
+    echo "User Name is required, please try again.<br />";
+}
+
+if (!isset($email1) || trim($email1) == '') {
+    echo "Email is required, please try again.<br />";
+}
+
+
+$db = new mysqli('localhost', 'testUser', 'cst126pass', 'cst126milestone');
+if (mysqli_connect_errno()) {
+    echo "<p>Error: Could not connect to database.<br/> Please try again later.</p>";
     exit;
 }
+$query = "INSERT INTO user_info (userName, firstname, middleName, lastName, nickName, email1, email2, address1, 
+                      address2, city, state, zipCode, country) 
+                      VALUES('$userName', '$firstName', '$middleName', '$lastName', '$nickName', '$email1', '$email2',
+                             '$address1', '$address2', '$city', '$state', '$zipCode', '$country')";
 
-
-if (!$nickName){
-    $nickName = $firstName+$lastName;
-}
-if($userRole === "Writer"){
-    $userRoleId = 1;
-}else{
-    $userRoleId = 2;
-}
-
-
-
-$db = new mysqli('localhost', $dbUserName, $dbPass, $dbName);
-if (mysqli_connect_error()) {
-    echo '<p> Error: Could not connect to database. <br />
-    Please try again later.</p>';
-    exit;
+if (mysqli_query($db, $query)) {
+    echo "New record created successfully";
+} else {
+    echo "Error: " . $query . "<br>" . mysqli_error($db);
 }
 
-$createUserQuery = "INSERT INTO $dbName VALUES (?,?,?,?,?,?,?,?,?,?,?,?,?,?)";
-$stmt = $db->prepare($createUserQuery);
-$stmt->bind_param('sssssssssssisi', $firstName, $middleName, $lastName,
-$nickname, $userName, $email1, $email2, $address1, $address2, $city, $state, $zipCode, $country, $userRoleId);
-$stmt->execute();
-
-if ($stmt->affected_rows > 0) {
-    echo "<p> User created successfully.</p>";
-}else{
-    echo "<p> An error has occured. <br />
-    The item was not added.</p>p";
-}
-
-$db->close();
+mysqli_close($db);
 
 ?>
 </body>
